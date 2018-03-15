2e6f5512e9b3a68ed0b2aef6b55b8217df85e811
bongla316 <abdullah.safdar@gmail.com>
2018-03-15 15:20:35 +0000

    Cleaned up excess code

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ document.querySelector("#s").addEventListener("click", playerScissors);
 
 
 function playerRock() {
-    console.log(playRound("ROCK"));
+    playRound("ROCK");
 }
 
 function playerPaper() {
-    console.log(playRound("PAPER"));
+    playRound("PAPER");
 }
 
 function playerScissors() {
-    console.log(playRound("SCISSORS"));
+    playRound("SCISSORS");
 }
 
 function computerPlay() {
@@ -34,30 +34,16 @@ function computerPlay() {
 function playRound(playerSelection) {
 
     var computerSelection = computerPlay().toUpperCase();
-    console.log(computerSelection);
 
     if (playerSelection == computerSelection) {
         displayResult(playerSelection, computerSelection, 0);
-        return "The round was a draw!";
     }
 
     else if ((playerSelection == "ROCK" && computerSelection == "SCISSORS") ||
         (playerSelection == "PAPER" && computerSelection == "ROCK") ||
         (playerSelection == "SCISSORS" && computerSelection == "PAPER")) {
 
-        //if (playerSelection == "ROCK") {
             displayResult(playerSelection, computerSelection, 1);
-            //return "Rock beats Scissors, Player wins!";
-        //}
-
-        //if (playerSelection == "PAPER") {
-            
-            //return "Paper beats Rock, Player wins!";
-        //}
-
-        //if (playerSelection == "SCISSORS") {
-            //return "Scissors beats Paper, Player wins!";
-        //}
 
     }
 
@@ -67,18 +53,6 @@ function playRound(playerSelection) {
 
             displayResult(playerSelection, computerSelection, 2);
 
-        //if (computerSelection == "ROCK") {
-            //return "Rock beats Scissors, Computer wins!";
-        //}
-
-        //if (computerSelection == "PAPER") {
-            //return "Paper beats Rock, Computer wins!";
-        //}
-
-        //if (computerSelection == "SCISSORS") {
-            //return "Scissors beats Paper, Computer wins!";
-        //}
-
     }
 }
 
@@ -86,19 +60,28 @@ function displayResult(playerChoice, computerChoice, winner) {
 
     if (winner == 0){
         var result = "The round was a draw";
+        var node = document.createElement("p");
         var textNode = document.createTextNode(result);
-        document.getElementById("displayResult").appendChild(textNode);
+        var textResult = node.appendChild(textNode);
+
+        document.getElementById("resultDisplay").appendChild(textResult);
     }
 
     else if (winner == 1){
         var result = playerChoice + " beats " + computerChoice + "! Player wins!";
+        var node = document.createElement("p");
         var textNode = document.createTextNode(result);
-        document.getElementById("displayResult").appendChild(textNode);
+        var textResult = node.appendChild(textNode);
+
+        document.getElementById("resultDisplay").appendChild(textResult);
     }
 
     else if (winner == 2){
         var result = computerChoice + " beats " + playerChoice + "! Computer wins!";
+        var node = document.createElement("p");
         var textNode = document.createTextNode(result);
-        document.getElementById("displayResult").appendChild(textNode);
+        var textResult = node.appendChild(textNode);
+
+        document.getElementById("resultDisplay").appendChild(textResult);
     }
 }
