5470f2a8f9302f2bd05728a76242067218e6b279
bongla316 <abdullah.safdar@gmail.com>
2018-03-15 15:57:16 +0000

    Added basic scoreboard to keep track of score and logged in console when the match finishes

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.querySelector("#r").addEventListener("click", playerRock);
 document.querySelector("#p").addEventListener("click", playerPaper);
 document.querySelector("#s").addEventListener("click", playerScissors);
 
+var computerScore = 0;
+var playerScore = 0;
+var currentRound = 0;
 
 function playerRock() {
     playRound("ROCK");
@@ -58,30 +61,61 @@ function playRound(playerSelection) {
 
 function displayResult(playerChoice, computerChoice, winner) {
 
+    currentRound += 1;
+
     if (winner == 0){
+        
         var result = "The round was a draw";
         var node = document.createElement("p");
         var textNode = document.createTextNode(result);
-        var textResult = node.appendChild(textNode);
+        node.appendChild(textNode);
 
-        document.getElementById("resultDisplay").appendChild(textResult);
+        document.getElementById("resultDisplay").appendChild(node);
     }
 
     else if (winner == 1){
         var result = playerChoice + " beats " + computerChoice + "! Player wins!";
         var node = document.createElement("p");
         var textNode = document.createTextNode(result);
-        var textResult = node.appendChild(textNode);
+        node.appendChild(textNode);
 
-        document.getElementById("resultDisplay").appendChild(textResult);
+        document.getElementById("resultDisplay").appendChild(node);
+        playerScore += 1;
+        
+        updateScoreboard(computerScore, playerScore);
+        winnerCheck(computerScore, playerScore);
+
     }
 
     else if (winner == 2){
         var result = computerChoice + " beats " + playerChoice + "! Computer wins!";
         var node = document.createElement("p");
         var textNode = document.createTextNode(result);
-        var textResult = node.appendChild(textNode);
+        node.appendChild(textNode);
 
-        document.getElementById("resultDisplay").appendChild(textResult);
+        document.getElementById("resultDisplay").appendChild(node);
+        computerScore += 1;
+
+        updateScoreboard(computerScore, playerScore);
+        winnerCheck(computerScore, playerScore);
     }
+}
+
+function winnerCheck(cScore, pScore){
+    if (cScore == 5) {
+        console.log("Computer wins the match!");
+    }
+
+    if (pScore == 5) {
+        console.log("Player wins the match!");
+    }
+
+    else {
+        return;
+    }
+}
+
+function updateScoreboard(cScore, pScore){
+    document.getElementById("scorePlayer").innerHTML = pScore;
+    document.getElementById("scoreComputer").innerHTML = cScore;
 }
